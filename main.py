4fcfe7c18a6712f091d557a72a486388be18ee69
Abdul-aziz9 <ooshinberu@gmail.com>
2024-10-11 07:25:01 -0400

    update for the score was made

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
 import turtle
 import pandas
 
+# Author: AbdulAziz Oshinberu
+# Date: 10/02/2024
+# This application is helps people struggling with identifying the name of states in the U.S.
+# Being an international student from Nigeria where the name of states are taught at an early stage in our educational system.
+# Decided to use this game to teach myself the name of states.
+# ENJOY!!
 
+
+# This part of the code displays the screen of the application(The MAP of the U.S) 
 screen = turtle.Screen()
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+# This part of the code added the name of the states to the program
 data = pandas.read_csv('50_states.csv')
 state_list = data.state.to_list()
 
-
-# def get_mouse_click(x, y):
-#     print(x, y)
-# turtle.onscreenclick(get_mouse_click) to get co-ordinates
+# This part of the code prepare to start the game and get every turtle ready
 game_on = True
 pet = turtle.Turtle()
 pet.ht()
 pet.penup()
 answer_given = []
 
+# This is where the code begins
 while game_on:
     score = len(answer_given)
-    answer = screen.textinput(f"{score}/50 Correct State", "What's Another State Name").title()
-    if answer == "Exit":
+    answer = screen.textinput(f"{score}/50 Correct State", "Give A State Name").title()
+    # This is the indentation for when the person leaves the game
+    if answer.lower == "exit":
         samp = data.state.to_list()
         # list comprehended below
         state_dic = {'states you didn\'t mention': [state for state in samp if state not in answer_given]}
@@ -32,6 +40,7 @@ while game_on:
         nudata = pandas.DataFrame(state_dic)
         nudata.to_csv('states_to_learn.csv')
         break
+    # This is the indentation for when the user is giving a name of a state 
     if answer in state_list:
         answer_given.append(answer)
         print(answer_given)
@@ -40,7 +49,8 @@ while game_on:
         y = int(row.y)
         pet.goto(x, y)
         pet.write(f'{row.state.item()}', font=('Courier', 10, 'normal'))
-        if score == 5:
-            pet.write('Completed', font=('Courier', 10, 'normal'))
+        if score == 50:
+            pet.goto(50, 100)
+            pet.write('Completed', font=('Times New Roman', 60, 'normal'))
             game_on = False
 turtle.mainloop()
